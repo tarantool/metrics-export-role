1b4db43dfa0db3d8edd84a69db4fb512120d2854
Maksim Tiushev <mandesero@gmail.com>
2026-08-06 15:35:48 +0000

    roles: stop removed export targets on reload
    Stop export targets missing from the new role configuration before
    applying the remaining targets.
    
    Closes #47

--- test/integration/reload_config_test.lua
+++ test/integration/reload_config_test.lua
@@ -157,6 +157,38 @@ g.test_reload_config_routes_exists = function(cg)
     t.assert(response.body)
 end
 
+g.test_reload_config_remove_http_target = function(cg)
+    cg.server = server:new({
+        config_file = fio.pathjoin(cg.workdir, 'config.yaml'),
+        chdir = cg.workdir,
+        alias = 'master',
+        workdir = cg.workdir,
+    })
+    cg.server:start({wait_until_ready = true})
+    t.assert(is_tcp_connect('127.0.0.1', 8082))
+
+    local config_path = fio.pathjoin(cg.workdir, 'config.yaml')
+    local file = fio.open(config_path, {'O_RDONLY'})
+    t.assert(file ~= nil)
+    local cfg = yaml.decode(file:read())
+    file:close()
+
+    local role_cfg = cfg.groups['group-001'].replicasets['replicaset-001'].
+        instances.master.roles_cfg['roles.metrics-export']
+    role_cfg.http = nil
+
+    file = fio.open(config_path, {
+        'O_CREAT', 'O_WRONLY', 'O_TRUNC',
+    }, tonumber('644', 8))
+    t.assert(file ~= nil)
+    file:write(yaml.encode(cfg))
+    file:close()
+
+    local _, err = cg.server:eval("require('config'):reload()")
+    t.assert_not(err)
+    t.assert_not(is_tcp_connect('127.0.0.1', 8082))
+end
+
 local function change_graphite_port_in_config(cg, new_port)
     if new_port == nil then
         new_port = '2222'
